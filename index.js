5bb54ea6e7cdd8ec6a389fa7d02581efa053421f
Gaston Schvarchman <janeleker@gmail.com>
2021-06-20 23:39:52 -0300

    Cambio index principal por cambios de pantalla

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const {app,BrowserWindow,Menu,ipcMain} = require('electron');
+const { app, BrowserWindow, Menu, ipcMain } = require('electron');
 const url = require('url')
 const path = require('path')
 const _ = require('lodash')
@@ -15,22 +15,46 @@ if (process.env.NODE_ENV !== 'production') {
     })
 }
 
+app.on('ready', () => {
 
-app.on('ready',() => {
-  
-  mainScreen = new BrowserWindow({
-    show:false,
-    webPreferences
-  })
-  mainScreen.loadURL(url.format({
-    pathname:path.join(__dirname,'views','mainScreen/index.html'),
-    protocol:'file',
-    slashes:true
-  }))
-  mainScreen.maximize()
-  mainScreen.once('ready-to-show',() => {mainScreen.show()})
-  ipcMain.on('buttonClick:restart',(event,data) => {
-    mainScreen.reload()
-  })
+    landingScreen = new BrowserWindow({
+      webPreferences
+    })
+    landingScreen.loadURL(url.format({
+      pathname: path.join(__dirname,'views','landingScreen/index.html'),
+      protocol:'file',
+      slashes:true
+    }))
+    landingScreen.maximize()
+    landingScreen.once('ready-to-show', () => { landingScreen.show() })
+    ipcMain.on('screens:battleScreen', (event, data) => {
+        battleScreen = new BrowserWindow({
+            show: false,
+            webPreferences
+        })
+        battleScreen.loadURL(url.format({
+            pathname: path.join(__dirname, 'views', 'battleScreen/index.html'),
+            protocol: 'file',
+            slashes: true
+        }))
+        battleScreen.maximize()
+        battleScreen.once('ready-to-show', () => { battleScreen.show() })
+        ipcMain.on('buttonClick:restart', (event, data) => {
+            battleScreen.reload()
+        })
+    })
+    ipcMain.on('screens:configurationScreen',(event,data) => {
+        configurationScreen = new BrowserWindow({
+            webPreferences,
+            width:400,
+            height:247,
+            frame:false      
+        })
+        configurationScreen.loadURL(url.format({
+            pathname:path.join(__dirname,'views','configurationScreen/index.html'),
+            protocol:'file',
+            slashes:true
+        }))
+    })
+
 })
-
