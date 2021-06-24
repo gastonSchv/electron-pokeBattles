361751c0424476d543a08cc03a1e776045b467dd
Gaston Schvarchman <janeleker@gmail.com>
2021-06-24 01:53:46 -0300

    Agrego tick de guardado

--- views/configurationScreen/script.js
+++ views/configurationScreen/script.js
@@ -3,12 +3,31 @@ const store = new Store()
 const { ipcRenderer } = require('electron')
 
 function guardarRutaPokemon() {
+    const tickGuardadoCorrecto = document.getElementById('tickGuardadoCorrecto')
     const valorInputRuta = inputRuta.value
     if (valorInputRuta) {
         store.set('ruta', { inputRuta: valorInputRuta })
     }
+ 	aparecerYDesvanecer(tickGuardadoCorrecto)	   
 }
-
+function aparecerYDesvanecer(htmlComponent, pace = 0.1) {
+    var opacity = 0;
+    var variationOpacityPace = pace;
+    var counter = 1;
+    var intervalId = setInterval(function() {
+        if (counter / 2 == 1 / variationOpacityPace) {
+            clearInterval(intervalId)
+        }
+        if (counter > 1 / variationOpacityPace) {
+            opacity -= variationOpacityPace
+            htmlComponent.style.opacity = opacity
+        } else {
+            opacity += variationOpacityPace
+            htmlComponent.style.opacity = opacity
+        }
+        counter++;
+    }, 70)
+}
 function functionesDeInicio() {
     actualizarPlaceHolder();
     notificarAltaDeScreenHaciaLanding()
