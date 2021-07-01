ff60f58b11bcbbcc45272a9f79c3111e63d55abd
Gaston Schvarchman <janeleker@gmail.com>
2021-06-30 21:39:05 -0300

    Agrego flujo main-renderer para juez de batalla

--- index.js
+++ index.js
@@ -5,16 +5,18 @@ const _ = require('lodash')
 const reload = require('electron-reload')
 
 let battleScreen = {}
+let juezDeBatallaScreen = {}
 const webPreferences = {
     nodeIntegration: true,
     contextIsolation: false
 }
 const defaultBrowserWindowSetting = {
-	webPreferences,
-	icon: 'C:/Users/Producteca/Desktop/Gaston/Programming learning/Electron/electron-pokeBattles/assets/icons/win/Bolbasaur.ico'
+    webPreferences,
+    icon: 'C:/Users/Producteca/Desktop/Gaston/Programming learning/Electron/electron-pokeBattles/assets/icons/win/Bolbasaur.ico'
 }
-function pathFromViewsDir(pathName){
-    return path.join(__dirname, 'views',pathName)
+
+function pathFromViewsDir(pathName) {
+    return path.join(__dirname, 'views', pathName)
 }
 if (process.env.NODE_ENV !== 'production') {
     reload(__dirname, {
@@ -22,17 +24,26 @@ if (process.env.NODE_ENV !== 'production') {
     })
 }
 
-function newScreen(browserWindowSettings,pathname){
+function ifUpSendConfigRuta(browserWindows,data) {
+    _.forEach(browserWindows, browserWindow => {
+        if (!_.isEmpty(browserWindow)) {
+            browserWindow.webContents.send('config:ruta', data)
+        }
+    })
+}
+
+function newScreen(browserWindowSettings, pathname) {
     const screen = new BrowserWindow({
         ...browserWindowSettings,
         ...defaultBrowserWindowSetting
     })
     screen.loadURL(url.format({
         pathname,
-        protocol:'file'
+        protocol: 'file'
     }))
     return screen
 }
+
 function newBattleScreen() {
     battleScreen = new BrowserWindow({
         show: false,
@@ -45,49 +56,49 @@ function newBattleScreen() {
 }
 app.on('ready', () => {
 
-    landingScreen = newScreen({},pathFromViewsDir('landingScreen/index.html'))
+    landingScreen = newScreen({}, pathFromViewsDir('landingScreen/index.html'))
     landingScreen.maximize()
     landingScreen.once('ready-to-show', () => { landingScreen.show() })
 
     configurationScreen = newScreen({
-        show: false,
-         width: 600,
-         height: 370,
-         frame: false},
-         pathFromViewsDir('configurationScreen/index.html')
+            show: false,
+            width: 600,
+            height: 370,
+            frame: false
+        },
+        pathFromViewsDir('configurationScreen/index.html')
     )
     ipcMain.on('buttonClick:restart', (event, data) => {
-        console.log('se apreto restart')
         battleScreen.reload()
     })
     ipcMain.on('config:ruta', (event, data) => {
-        console.log('llego ruta a main', data)
-        battleScreen.webContents.send('config:ruta', data)
+        ifUpSendConfigRuta([battleScreen, juezDeBatallaScreen],data)
     })
     ipcMain.on('screens:battleScreen', (event, data) => {
         newBattleScreen()
         battleScreen.maximize()
         battleScreen.show()
     })
-    ipcMain.on('config:pedidoRuta',(event,data) => {
+    ipcMain.on('config:pedidoRutaBattleScreen', (event, data) => {
+        configurationScreen.webContents.send('config:ruta', data)
+    })
+    ipcMain.on('config:pedidoRutaJuezDeBatallaScreen', (event, data) => {
         configurationScreen.webContents.send('config:ruta', data)
     })
     ipcMain.on('screens:configurationScreen', (event, data) => {
         configurationScreen.show()
     })
-    ipcMain.on('screens:juezDeBatallaScreen',(event,data) => {
-        console.log('llego juezDeBatallaScreen')
-        juezDeBatallaScreen = newScreen({frame: false},pathFromViewsDir('juezDeBatallaScreen/index.html'))
-        juezDeBatallaScreen.setPosition(250,60)
+    ipcMain.on('screens:juezDeBatallaScreen', (event, data) => {
+        juezDeBatallaScreen = newScreen({ frame: false }, pathFromViewsDir('juezDeBatallaScreen/index.html'))
+        juezDeBatallaScreen.setPosition(250, 60)
     })
     ipcMain.on('screens:configurationScreenHide', (event, data) => {
-        console.log('deberia ocultar configurationScreen')
         configurationScreen.hide()
     })
-    ipcMain.on('altaDeScreen:configuracion',(event,data) => {
-        landingScreen.webContents.send('altaDeScreen:configuracion',{})
+    ipcMain.on('altaDeScreen:configuracion', (event, data) => {
+        landingScreen.webContents.send('altaDeScreen:configuracion', {})
     })
-    landingScreen.on('close', (event,data) => {
+    landingScreen.on('close', (event, data) => {
         app.quit()
     })
 })
