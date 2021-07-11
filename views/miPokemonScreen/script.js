db9c91850297a2456bde1cce8b3f7eeeaf18714c
Gaston Schvarchman <janeleker@gmail.com>
2021-07-11 13:58:00 -0300

    Hago dinámicos los valores de estadisticas

--- views/miPokemonScreen/script.js
+++ views/miPokemonScreen/script.js
@@ -11,7 +11,9 @@ function pedirRutaConfig(){
 }
 ipcRenderer.on('config:pedidoRutaMiPokemon',(event,data) => {
 	let pokemon = require(data.ruta)
+	console.log(pokemon)
 	completarEstadoPokemon(pokemon)
+	completarValoresDeEstadisticas(pokemon)
 })
 
 function completarEstadoPokemon(unPokemon){
@@ -19,20 +21,22 @@ function completarEstadoPokemon(unPokemon){
 }
 
 function completarValorDeDato(div,campo,unPokemon){
-	console.log(campo,_.get(unPokemon,`${campo}.nombre`),campo.nombre)
 	if(_.get(unPokemon,`${campo}.nombre`)){campo = `${campo}.nombre`}   
 	div.innerHTML = `<span class="textoDatoPokemon">${_.get(unPokemon,campo)}</span>`
 }
 function completarValoresDeDato(unPokemon){
-	const nombre = document.getElementById('nombre')
-	const entrenador = document.getElementById('entrenador')
-	const tipoDePokemon = document.getElementById('tipoDePokemon')
-	const evolucion = document.getElementById('evolucion')
-	
-	console.log(nombre,entrenador,tipoDePokemon,evolucion)
-	
 	completarValorDeDato(nombre,'nombre',unPokemon)
 	completarValorDeDato(entrenador,'entrenador',unPokemon)
 	completarValorDeDato(tipoDePokemon,'tipoDePokemon',unPokemon)
 	completarValorDeDato(evolucion,'evolucion',unPokemon)
+}
+function completarValorDeEstadistica(div,campo,unPokemon){
+	div.innerHTML = _.get(unPokemon,campo)
+}
+function completarValoresDeEstadisticas(unPokemon){
+	completarValorDeEstadistica(valorVida,'vida',unPokemon)
+	completarValorDeEstadistica(valorEnergia,'energia',unPokemon)
+	completarValorDeEstadistica(valorFuerza,'fuerza',unPokemon)
+	completarValorDeEstadistica(valorDefensa,'defensa',unPokemon)
+	completarValorDeEstadistica(valorVelocidad,'velocidad',unPokemon)
 }
