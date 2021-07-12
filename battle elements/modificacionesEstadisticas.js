0db3acef01bbf6bcf41d376a341b6aa959be65d7
Gaston Schvarchman <janeleker@gmail.com>
2021-07-12 01:13:12 -0300

    Rename de archivo modificaciones estadisticas

---
+++ battle elements/modificacionesEstadisticas.js
@@ -0,0 +1,39 @@
+module.exports = [{
+        nombrePokemon: 'bolbasaur',
+        atributos: [{
+                atributo: 'vida',
+                valor: 40000,
+            },
+            {
+                atributo: 'fuerza',
+                valor: 1500
+            },
+            {
+                atributo: 'energia',
+                valor: 1000
+            },
+            {
+                atributo: 'energiaLimite',
+                valor: 300
+            },
+            {
+                atributo: 'defensa',
+                valor: 450
+            }
+        ],
+        ataques: ['fuete', 'maximo']
+    },
+    {
+        nombrePokemon: 'charmander',
+        atributos: [{
+                atributo: 'vida',
+                valor: 200,
+            },
+            {
+                atributo: 'defensa',
+                valor: 300
+            }
+        ],
+        ataques: ['fuete', 'maximo']
+    }
+]
