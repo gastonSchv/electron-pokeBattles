80c1c4df3ec5f21ba3ca52a4ae28e5bbc96f4501
Gaston Schvarchman <janeleker@gmail.com>
2021-07-19 22:52:45 -0300

    Muevo los pokemons

---
+++ battle elements/Pokemons/charmander.js
@@ -0,0 +1,18 @@
+const juan = require('../entrenadorJuan')
+const Pokemon = require('./Pokemon')
+const tipoDePokemon = require('../TiposDePokemon/fuego')
+
+ const pokemon = new Pokemon({
+	entrenador: juan,
+	nombre:'charmander',
+	tipoDePokemon,
+	evolucion:1,
+	vida:500,// hacer pruebas para ver porque rompe por condicion de ataque cuando cambio por otros valores
+	energia:1000,
+	fuerza:1000,
+	defensa:1000,
+	velocidad:1000
+})
+
+
+module.exports = pokemon
