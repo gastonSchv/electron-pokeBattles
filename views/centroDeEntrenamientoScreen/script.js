a909fd06a6182e2860317cfeb48b5f5f87fd4dfb
Gaston Schvarchman <janeleker@gmail.com>
2021-08-12 21:59:20 -0300

    Manejo UI de entrenamientos con juezDeEntrenamiento

--- views/centroDeEntrenamientoScreen/script.js
+++ views/centroDeEntrenamientoScreen/script.js
@@ -2,7 +2,7 @@ const juezDeEntrenamiento = require('../../training management/juezDeEntrenamien
 const util = require('../utils/util')
 const entrenamientos = require('../../training management/entrenamientos')
 const _ = require('lodash')
-const {ipcRenderer} = require('electron')
+const { ipcRenderer } = require('electron')
 let pokemon = {}
 
 function funcionesDeInicio() {
@@ -10,15 +10,17 @@ function funcionesDeInicio() {
     agregarEntrenamientos();
     pedirRutaConfig();
 }
-function pedirRutaConfig(){
-    ipcRenderer.send('config:pedidoRutaCentroDeEntrenamiento',{})
+
+function pedirRutaConfig() {
+    ipcRenderer.send('config:pedidoRutaCentroDeEntrenamiento', {})
 }
-ipcRenderer.on('config:pedidoRutaCentroDeEntrenamiento',(event,data) => {
+ipcRenderer.on('config:pedidoRutaCentroDeEntrenamiento', (event, data) => {
     pokemon = require(data.ruta)
 
 })
-function agregarEntrenamientos(){
-    _.forEach(entrenamientos,agregarEntrenamiento)
+
+function agregarEntrenamientos() {
+    _.forEach(entrenamientos, agregarEntrenamiento)
 }
 
 function cerrarPantalla() {
@@ -27,8 +29,8 @@ function cerrarPantalla() {
 
 function premioshtml(premios) {
     const __premioEntrenamientoHtml = premio => {
-    	const {habilidad,valor} = premio;
-    	return `<div class="premioEntrenamiento">
+        const { habilidad, valor } = premio;
+        return `<div class="premioEntrenamiento">
 				<div class="tituloPremioDiv">
 				    <p class="tituloPremio">${habilidad.toUpperCase()}</p>
 				</div>
@@ -37,11 +39,11 @@ function premioshtml(premios) {
 				</div>
 			</div>`
     }
-    return _.map(premios,__premioEntrenamientoHtml).join("")
+    return _.map(premios, __premioEntrenamientoHtml).join("")
 }
 
 function agregarEntrenamiento(entrenamiento) {
-    const { id,titulo, premios, descripcion } = entrenamiento
+    const { id, titulo, premios, descripcion } = entrenamiento
     entrenamientosGrid.innerHTML += `<div id="${entrenamiento.id}" class="entrenamiento">
                 <div class="tituloEntrenamientoDiv">
                     <p class="tituloEntrenamiento">${titulo}</p>
@@ -52,33 +54,69 @@ function agregarEntrenamiento(entrenamiento) {
                 <div class="descripcionEntrenamientoDiv">
                     <p class="descripcionEntrenamiento">${descripcion}</p>
                 </div>
-                <div class="botonEntrenamientoDiv">
+                <div id="boton${entrenamiento.id}Div" class="botonEntrenamientoDiv">
                     <button id="boton${entrenamiento.id}" onclick="constatarEntrenamiento('${entrenamiento.id}')" class="btn btn-primary">
                         Realizar entrenamiento
                     </button>
                 </div>
             </div>`
-    if(juezDeEntrenamiento.hizoElEntrenamiento(entrenamiento.id)){
-        bloquearEntrenamiento(entrenamiento.id)
+    if (juezDeEntrenamiento.hizoElEntrenamiento(entrenamiento.id)) {
+    	cambiarEntrenamientoRealizado(entrenamiento.id)
     }
 }
-function bloquearEntrenamiento(entrenamientoId){
-    const boton = document.getElementById(`boton${entrenamientoId}`)
+function cambiarEntrenamientoRealizado(entrenamientoId){
+	cambiarBloqueoEntrenamiento(entrenamientoId)
+    cambiarBotonAccionEnTarjeta(entrenamientoId)
+}
+function cambiarBotonAccionEnTarjeta(entrenamientoId) {
+    const botonEntrenamientoDiv = document.getElementById(`boton${entrenamientoId}Div`)
+    console.log(juezDeEntrenamiento.hizoElEntrenamiento(entrenamientoId),entrenamientoId)
+    if (juezDeEntrenamiento.hizoElEntrenamiento(entrenamientoId)) {
+        botonEntrenamientoDiv.innerHTML = `
+		<button id="botonBorrar${entrenamientoId}" onclick="borrarEntrenamientoDeStore('${entrenamientoId}')" class="btn btn-danger">
+    	    Restaurar entrenamiento
+   		 </button>`
+    }else{
+    	botonEntrenamientoDiv.innerHTML = `
+    	<button id="boton${entrenamientoId}" onclick="constatarEntrenamiento('${entrenamientoId}')" class="btn btn-primary">
+        	Realizar entrenamiento
+        </button>`
+    }
+}
+function cambiarBloqueoEntrenamiento(entrenamientoId){
+	const botonRealizarEntrenamiento = document.getElementById(`boton${entrenamientoId}`)
     const entrenamiento = document.getElementById(entrenamientoId)
 
-    entrenamiento.style.opacity = 0.3
-    boton.disabled = true
+	if(juezDeEntrenamiento.hizoElEntrenamiento(entrenamientoId)){
+		console.log('entre a cambiar, entrenamiento: ', entrenamiento)
+		entrenamiento.style.opacity = 0.3
+    	botonRealizarEntrenamiento.opacity = 0
+    	botonRealizarEntrenamiento.disabled = true
+	}else{
+		entrenamiento.style.opacity = 1
+    	botonRealizarEntrenamiento.opacity = 1
+    	botonRealizarEntrenamiento.disabled = false
+	}
 }
-function guardarEntrenamientoExistoso(nombrePokemon,entrenamientoId){
-	ipcRenderer.send('guardarEntrenamiento',{nombrePokemon,entrenamientoId}) 
+function guardarEntrenamientoExistoso(nombrePokemon, entrenamientoId) {
+    juezDeEntrenamiento.guardarEntrenamientoExistoso({ nombrePokemon, entrenamientoId })
 }
+
+function borrarEntrenamientoDeStore(entrenamientoId) {
+    juezDeEntrenamiento.borrarEntrenamientoRealizado({ entrenamientoId })
+    cambiarBotonAccionEnTarjeta(entrenamientoId)
+    cambiarBloqueoEntrenamiento(entrenamientoId)
+}
+
 function constatarEntrenamiento(entrenamientoId) {
     try {
         juezDeEntrenamiento.constatarEntrenamiento(pokemon, entrenamientoId);
-        guardarEntrenamientoExistoso(pokemon.nombre,entrenamientoId);
-        bloquearEntrenamiento(entrenamientoId);
+        guardarEntrenamientoExistoso(pokemon.nombre, entrenamientoId);
+        sonidoEntrenamientoCorrecto.play();
+        cambiarEntrenamientoRealizado(entrenamientoId);
         /*mostrarResultadoExitoso(entrenamiento);*/
     } catch (err) {
+        sonidoEntrenamientoIncorrecto.play()
         console.log('Error! ', err)
         /*mostrarResultadoFallido(entrenamiento,err)*/
     }
