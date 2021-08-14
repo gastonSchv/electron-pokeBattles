c7df51fa1cdf6328bef7933462b7de0a57cd404f
Gaston Schvarchman <janeleker@gmail.com>
2021-08-13 23:19:59 -0300

    Manejo enemigos derrotados

--- views/selectorDeEnemigoScreen/script.js
+++ views/selectorDeEnemigoScreen/script.js
@@ -4,24 +4,26 @@ const path = require('path')
 const _ = require('lodash')
 const carpetaPokemonsEnemigos = path.join(__dirname,'..','..','battle elements','Pokemons','Pokemons enemigos')
 const util = require('../utils/util')
+const juezDeBatalla = require('../../battle elements/juezDeBatalla')
 
 function getButton(button){
-	ipcRenderer.send('screens:battleScreen',{enemigoSeleccionado:button.id})	
+	ipcRenderer.send('screens:battleScreen',{enemigoSeleccionado:button.id})
 }
 function funcionesDeInicio(){
 	crearTodosLosBotonesEnemigos();
 	util.crearBotonCerradoConEstilo(contenedor)
+	pedirPokemonesDerrotados()
+}
+function pedirPokemonesDerrotados(){
+	ipcRenderer.send('pedidoPokemonesDerrotados',{})
 }
 function cerrarPantalla(){
 	window.close()
 }
 function crearBotonEnemigo(nombrePokemonEnemigo){
-	console.log(nombrePokemonEnemigo);
 	enemigosGrid.innerHTML +=  `<button onclick="getButton(this)" id="${nombrePokemonEnemigo}" class="enemigo">
 	<img class="imagenEnemigo" src="../../../assets/images/${nombrePokemonEnemigo}.png">
 	</button>`
-
-	console.log(enemigosGrid)
 }
 function obtenerNombrePokemonDesdeNombreArchivo(nombreArchivo){
 	return _.head(_.split(nombreArchivo,'.'))
@@ -33,3 +35,16 @@ function crearTodosLosBotonesEnemigos(){
 	  	});
 	});
 }
+function cambiarBotonPokemonDerrotado(nombrePokemonDerrotado){
+	const boton = document.getElementById(nombrePokemonDerrotado)
+	boton.className = "pokemonDerrotado"
+}
+ipcRenderer.on('pokemonesDerrotados',(event,data) => {
+	const {pokemonesDerrotados} = data;
+	if(!_.isEmpty(pokemonesDerrotados)){
+		_.forEach(pokemonesDerrotados,cambiarBotonPokemonDerrotado)
+	}
+})
+ipcRenderer.on('avisoPokemonDerrotado',(event,data) => {
+	cambiarBotonPokemonDerrotado(data.nombrePokemonDerrotado)
+})
