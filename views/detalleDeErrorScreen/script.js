ea746c8ae62c39034ee6de22d3ecf00bcfd742c7
Gaston Schvarchman <janeleker@gmail.com>
2021-08-30 00:47:05 -0300

    Manejo titulo de error y recomendaciones

--- views/detalleDeErrorScreen/script.js
+++ views/detalleDeErrorScreen/script.js
@@ -1,4 +1,6 @@
 const util = require('../utils/util')
+const { ipcRenderer } = require('electron')
+const _ = require('lodash')
 
 function funcionesDeInicio(){
     util.crearBotonCerradoConEstilo(contenedor)
@@ -8,3 +10,22 @@ function funcionesDeInicio(){
 function cerrarPantalla(){
     window.close()
 }
+function agregarError(errMessage){
+	tituloDeError.innerHTML = errMessage
+}
+function agregarRecomendacion(recomendacion){
+	listaDeRecomendaciones.innerHTML += `
+			<li class="recomendacion">
+			 ${recomendacion}	
+			</li>			
+	`
+}
+function agregarRecomendaciones(recomendaciones){
+	const recomendacionesList = recomendaciones.split('*')
+	_.forEach(recomendacionesList, agregarRecomendacion)
+}
+ipcRenderer.on('detalleDeError',(event,data) => {
+	 agregarError(data.errMessage)
+	 agregarRecomendaciones(data.recommendations)
+})
+
