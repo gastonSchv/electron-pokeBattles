ad6597f59880018e010d9f1fce2fc8589ce88c55
Gaston Schvarchman <janeleker@gmail.com>
2021-09-12 16:13:16 -0300

    Creo pokemons para pruebas

---
+++ battle elements/pokemons para pruebas/squartle para pruebas - copia.js
@@ -0,0 +1,20 @@
+const Pokemon = require('./Pokemons/Pokemon')
+const tipoDePokemon = require('./TiposDePokemon/agua')
+const gasti = require('./entrenadorGasti')
+const _ = require('lodash')
+
+const pokemon = new Pokemon({
+	entrenador: gasti,
+	nombre:'squartle',
+	tipoDePokemon,
+	evolucion:1,
+	vida:1000,
+	energia:1000,
+	fuerza:1000,
+	defensa:1000,
+	velocidad:1000
+})
+pokemon.miNombre = () => pokemon.nombre
+pokemon.elPokemonMasFuerte = unosPokemones => _.maxBy(unosPokemones, pokemon => pokemon.fuerza)
+pokemon.obtenerAtributos = pokemon => asd
+module.exports = pokemon
