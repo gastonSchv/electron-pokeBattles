a1a0eb5a224e401973f939fd13021df2a1bff535
Gaston Schvarchman <janeleker@gmail.com>
2021-09-17 19:08:29 -0300

    Agrego nuevos pokemons enemigos

---
+++ battle elements/Pokemons/Pokemons enemigos/meowth.js
@@ -0,0 +1,18 @@
+const juan = require('../../entrenadorJuan')
+const Pokemon = require('../Pokemon')
+const tipoDePokemon = require('../../TiposDePokemon/normal')
+
+ const pokemon = new Pokemon({
+	entrenador: juan,
+	nombre:'meowth',
+	tipoDePokemon,
+	evolucion:1,
+	vida:2000,
+	energia:600,
+	fuerza:500,
+	defensa:800,
+	velocidad:1100
+})
+
+
+module.exports = pokemon
---
+++ battle elements/Pokemons/Pokemons enemigos/venonat.js
@@ -0,0 +1,18 @@
+const juan = require('../../entrenadorJuan')
+const Pokemon = require('../Pokemon')
+const tipoDePokemon = require('../../TiposDePokemon/bicho')
+
+ const pokemon = new Pokemon({
+	entrenador: juan,
+	nombre:'venonat',
+	tipoDePokemon,
+	evolucion:1,
+	vida:2000,
+	energia:600,
+	fuerza:500,
+	defensa:800,
+	velocidad:1100
+})
+
+
+module.exports = pokemon
