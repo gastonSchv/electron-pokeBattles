50a3df2f0a8529e82a5aae9e3b1d663cef7d8f2a
Gaston Schvarchman <janeleker@gmail.com>
2021-09-26 20:38:57 -0300

    Saco metodos caducos

--- battle elements/Relator.js
+++ battle elements/Relator.js
@@ -4,6 +4,9 @@ class Relator {
 	constructor(){
 		this.mensajeFinDeBatalla = 'Fin de Batalla Pokemon';
 	}
+	stringificarYSepararComas(elemento){
+		return JSON.stringify(elemento).replaceAll(',',', ')
+	}
 	deListaComparacionesAString(listaComparaciones){
 		if(_.isEmpty(listaComparaciones)) return '';
 
@@ -18,17 +21,8 @@ class Relator {
 	anunciarValidacionCorrecta(unPokemon,condicion){
 		return ` \nEl pokemon ${unPokemon.nombre} ha verificado ${condicion} con exito!`
 	}
-	anunciarDesmayoPokemon(unPokemon){
-		console.log(` \nEl pokemon ${unPokemon.nombre} se ha desmayado`)	
-	}
 	estadoPokemon(unPokemon){
 		return `${unPokemon.nombre} vida: ${unPokemon.vida} | energia: ${unPokemon.energia} | daño recibido: ${unPokemon.dañoRecibido}`
-	}
-	anunciarResultadosDeRonda(unPokemon,otroPokemon,ronda){
-		const infoDeRonda = pokemon => {
-			return `\n\n ${this.estadoPokemon(pokemon)}`
-		}
-		console.log(`\n Resultados de la Ronda | ${ronda} | !`,infoDeRonda(unPokemon),infoDeRonda(otroPokemon))
 	}
 	anunciarCondicionesDeAtaqueFaltante(unPokemon,tipoDeAtaque){
 		this.anunciarFalta(unPokemon,`Condicion de ataque ${tipoDeAtaque}`)
@@ -49,14 +43,7 @@ class Relator {
 		return this.anunciarValidacionCorrecta(unPokemon,tipoDeEvaluacion)
 	}
 	anunciarEntrenamientoDesigual(resultadoEsperado,resultadoObtenido){
-		return `El entrenamiento no pudo completarse! Resultado esperado: ${resultadoEsperado} Resultado obtenido: ${resultadoObtenido}`
-	}
-	anunciarGanador(unPokemon,otroPokemon,ronda){
-		const pokemonGanador = _.find([unPokemon,otroPokemon], pokemon => pokemon.vida > pokemon.dañoRecibido)
-		const __estadoFinalPokemones = pokemones => {
-			return _.map(pokemones, pokemon => `\n\n ${this.estadoPokemon(pokemon)}`)
-		}
-		console.log(`\n ** El pokemon ${pokemonGanador.nombre} es el vencedor | ronda ${ronda} |** ${__estadoFinalPokemones([unPokemon,otroPokemon])}`)
+		return `Resultado esperado: ${this.stringificarYSepararComas(resultadoEsperado)} | Resultado obtenido: ${resultadoObtenido}`
 	}
 }
 
