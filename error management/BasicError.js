47f5175263df5c08a9db602b4611fb2eb63f5f35
Gaston Schvarchman <janeleker@gmail.com>
2021-10-03 00:24:44 -0300

    Agrego metodos de localizacion de error

--- error management/BasicError.js
+++ error management/BasicError.js
@@ -19,6 +19,20 @@ class BasicError {
 		}
 		return ''
 	}
+	errorPosition(){
+		const err = this.originalError
+		const [formato,linea,columna] = _.last(err.stack.split('at')[1].split('\\')).split('.')[1].replace(')','').trim().split(':')
+		return `${linea}*${columna}`
+	}
+	errorRoute(){
+		const err = this.originalError
+		const ruta = _.dropRight(_.slice(err.stack.split('at')[1].split('\\'),1).join('/').split('.'),1).join('')
+		return ruta
+	}
+	errorLocalization(){
+		const err = this.originalError
+		return `${this.errorPosition(err)}*${this.errorRoute(err)}`
+	}
 }
 
 module.exports = BasicError	;
