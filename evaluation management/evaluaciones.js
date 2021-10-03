0d24be23f635ae8ebdd9fe5609b62367d8d985bf
Gaston Schvarchman <janeleker@gmail.com>
2021-10-03 17:56:48 -0300

    Agrego nuevas evaluaciones

--- evaluation management/evaluaciones.js
+++ evaluation management/evaluaciones.js
@@ -2,5 +2,6 @@ const evaluacionBuilder = require('./evaluacionBuilder')
 
 module.exports = [
     evaluacionBuilder('atributosNecesarios'),
+    evaluacionBuilder('atributosConValoresPermitidos'),
     evaluacionBuilder('puntoDeAtributosMaximoPermitido')
 ]
--- evaluation management/informacionEvaluaciones.js
+++ evaluation management/informacionEvaluaciones.js
@@ -1,14 +1,17 @@
 module.exports = [
-{
-	id:'atributosNecesarios',
-	mensajeResultadoCorrecto: 'Tu pokemon cuenta con los atributos requeridos'
-}
-,{
-	id:'puntoDeAtributosMaximoPermitido',
-	mensajeResultadoCorrecto: 'Tu pokemon no excede el límite de puntos de atributos permitidos'
-},
-{
-	id:'existeMetodoAtacar',
-	mensajeResultadoCorrecto:'Tu pokemon cuenta con el metodo atacar(otroPokemon, tipoDeAtaque)'
-}
+	{
+        id: 'atributosNecesarios',
+        mensajeResultadoCorrecto: 'Tu pokemon cuenta con los atributos requeridos'
+    },
+    {
+        id: 'atributosConValoresPermitidos',
+        mensajeResultadoCorrecto: 'Tu pokemon cuenta con valores permitidos en todos sus atributos'
+    }, {
+        id: 'puntoDeAtributosMaximoPermitido',
+        mensajeResultadoCorrecto: 'Tu pokemon no excede el límite de puntos de atributos permitidos'
+    },
+    {
+        id: 'existeMetodoAtacar',
+        mensajeResultadoCorrecto: 'Tu pokemon cuenta con el metodo atacar(otroPokemon, tipoDeAtaque)'
+    }
 ]
