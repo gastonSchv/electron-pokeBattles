6ddf18deb0a923bcb3e8ef5c4062f8766a6b4d49
Gaston Schvarchman <janeleker@gmail.com>
2021-10-03 00:32:39 -0300

    Abstraigo logica de evaluaciones

--- views/juezDeBatallaScreen/script.js
+++ views/juezDeBatallaScreen/script.js
@@ -8,65 +8,71 @@ function funcionesDeInicio() {
     pedirRutaConfig()
     util.crearBotonCerradoConEstilo(contenedor)
 }
-function cerrarPantalla(){
+
+function cerrarPantalla() {
     window.close()
 }
+
 function pedirRutaConfig() {
     ipcRenderer.send('config:pedidoRutaJuezDeBatallaScreen', {})
 }
-function prenderPitidoArbitro(){
+
+function prenderPitidoArbitro() {
     pitidoArbitro.volume = 0.5
     pitidoArbitro.play()
 }
-function agregarEvaluacion(tipoDeEvaluacion, mensaje = '',tick) {
-     	contenedorEvaluaciones.innerHTML += `
-    <div id="botonError${tipoDeEvaluacion}" class="contenedorEvaluacion">
-        <div id="${tipoDeEvaluacion}" class="evaluaciones">
+
+
+function deshabilitarBotonesDeJuego(booleano) {
+    ipcRenderer.send('bloqueoBotonesDeJuego:juezDeBatalla', { deshabilitarBotones: booleano })
+}
+
+function habilitarDetalleResultadoFallido(idEvaluacion, err) {//agregar localizacion
+    const contenedorEvaluacion = document.getElementById(`botonError${idEvaluacion}`)
+    contenedorEvaluacion.innerHTML += `
+         <button id="botonError${idEvaluacion}" onclick="mostrarDetalleError('\
+         ${err.prettyMessage()}','${err.recommendations()}','${err.message()}')" class="botonDetalleError">
+            detalle error
+        </button>`
+}
+function realizarEvaluacion(unPokemon,idEvaluacion){
+	return evaluar(idEvaluacion, unPokemon, () => juezDeBatalla.constatarEvaluacion(unPokemon,idEvaluacion))	
+}
+function mostrarDetalleError(errMessage,recommendations,originalErrorMessage){//agregar localizacion
+    ipcRenderer.send('detalleDeError',{errMessage,recommendations,originalErrorMessage})
+}
+function realizarEvaluacionesNecesarias(unPokemon) {
+    realizarEvaluacion(unPokemon,'atributosNecesarios')
+    realizarEvaluacion(unPokemon,'puntoDeAtributosMaximoPermitido')
+}
+ipcRenderer.on('config:pedidoRutaJuezDeBatallaScreen', (event, data) => {
+    const unPokemon = require(`${data.ruta}`)
+    deshabilitarBotonesDeJuego(false)
+    realizarEvaluacionesNecesarias(unPokemon)
+    window.visualViewport.width > 400 ? prenderPitidoArbitro() : ''
+})
+
+function evaluar(idEvaluacion, unPokemon, evaluacionDeJuez) {
+
+    return evaluacionDeJuez(unPokemon)
+        .then(() => {
+            agregarEvaluacion(idEvaluacion, relator.anunciarEvaluacionCorrecta(unPokemon, idEvaluacion), 'tick verde')
+        })
+        .catch(err => {
+            agregarEvaluacion(idEvaluacion, err.prettyMessage(), 'tick rojo')
+            deshabilitarBotonesDeJuego(true)
+            habilitarDetalleResultadoFallido(idEvaluacion, err)
+        })
+}
+
+function agregarEvaluacion(idEvaluacion, mensaje = '', tick) {
+    contenedorEvaluaciones.innerHTML += `
+    <div id="botonError${idEvaluacion}" class="contenedorEvaluacion">
+        <div id="${idEvaluacion}" class="evaluaciones">
         <p class="mensajeEvaluacion">${util.substringSiHaceFalta(mensaje,200)}</p>
         </div>
         <div class="resultadoEvaluacion">
             <img src="../../../assets/images/${tick}.png">
         </div>
-	</div>`
+    </div>`
 }
-function deshabilitarBotonesDeJuego(booleano){
-	ipcRenderer.send('bloqueoBotonesDeJuego:juezDeBatalla',{deshabilitarBotones:booleano})
-}
-function evaluar(tipoDeEvaluacion, unPokemon, evaluacionDeJuez) {
-    try {
-        evaluacionDeJuez(unPokemon);
-        agregarEvaluacion(tipoDeEvaluacion, relator.anunciarEvaluacionCorrecta(unPokemon,tipoDeEvaluacion),'tick verde')
-    } catch (err) {
-        agregarEvaluacion(tipoDeEvaluacion, err.message,'tick rojo')
-        deshabilitarBotonesDeJuego(true)
-        habilitarDetalleResultadoFallido(tipoDeEvaluacion,err)
-    }
-}
-function habilitarDetalleResultadoFallido(tipoDeEvaluacion,err){
-		const contenedorEvaluacion = document.getElementById(`botonError${tipoDeEvaluacion}`)
-     	contenedorEvaluacion.innerHTML += `
-	     <button id="botonError${tipoDeEvaluacion}" onclick="mostrarDetalleError('${err.prettyMessage()}','${err.recommendations()}','${err.message()}')" class="botonDetalleError">
-        	detalle error
-        </button>`
-}
-function evaluarEstadoPokemon(unPokemon) {
-    evaluar('evaluacionEstado', unPokemon, () => juezDeBatalla.verificarEstadoPokemon(unPokemon))
-}
-function evaluarAtaqueBasico(unPokemon){
-	evaluar('evaluacionAtaqueBasico',unPokemon,() => juezDeBatalla.verificarAtaqueBasico(unPokemon))
-}
-function evaluarDanoDeAtaquesDisponibles(unPokemon){
-	evaluar('evaluacionDanoDeAtaquesDisponibles',unPokemon,() => juezDeBatalla.verificarDanoDeAtaquesDisponibles(unPokemon))
-}
-function realizarEvaluacionesNecesarias(unPokemon){
-	evaluarEstadoPokemon(unPokemon)
-    evaluarAtaqueBasico(unPokemon)
- 	evaluarDanoDeAtaquesDisponibles(unPokemon)
-}
-ipcRenderer.on('config:pedidoRutaJuezDeBatallaScreen', (event, data) => {
-
-    const unPokemon = require(`${data.ruta}`)
-    deshabilitarBotonesDeJuego(false)
-    realizarEvaluacionesNecesarias(unPokemon)    
-	window.visualViewport.width > 400? prenderPitidoArbitro():''
-})
