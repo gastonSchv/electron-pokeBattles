debb408ad1ab94d6efd7dc89315ed4aa4f151cd5
Gaston Schvarchman <janeleker@gmail.com>
2021-10-04 21:21:14 -0300

    Agrego lista de atributos

--- battle elements/config.js
+++ battle elements/config.js
@@ -1,37 +1,49 @@
 const _ = require('lodash')
 
 class config {
-	constructor(){
-	this.tiposDeAtaque = ['basico','fuerte','maximo'];	
-	this.ataquesVerificables = ['fuerte','maximo'];
-	this.multiplicadoresDeAtaque = {
-		basico : 5,
-		medio:10,
-		fuerte : 15,
-		maximo : 25
-	};
-	this.atributosDePokemon  =  ['vida','energia','fuerza','defensa','velocidad'];
-	this.puntajePorAtributo = 1000 ;
-	this.tiposDeDatoPorAtributo = {
-		entrenador:'string',
-		nombre:'string',
-		evolucion:'number',
-		vida:'number',
-		energia:'number',
-		energiaLimite:'number',
-		fuerza:'number',
-		defensa:'number',
-		velocidad:'number',
-		dañoRecibido:'number'
-	}
-	this.listaPropiedadesDeEstado = ['entrenador','nombre','evolucion','vida','energia','energiaLimite','fuerza','defensa','velocidad','dañoRecibido']; 	
-	}
-	puntajeMaximoPermitido(){
-		return this.atributosDePokemon.length * this.puntajePorAtributo
-	}
-	multiplicadorDeAtaque(tipoDeAtaque){
-		return _.get(this.multiplicadoresDeAtaque,tipoDeAtaque)
-	}
+    constructor() {
+        this.tiposDeAtaque = ['basico', 'fuerte', 'maximo'];
+        this.ataquesVerificables = ['fuerte', 'maximo'];
+        this.multiplicadoresDeAtaque = {
+            basico: 5,
+            medio: 10,
+            fuerte: 15,
+            maximo: 25
+        };
+        this.atributosDePokemon = ['vida', 'energia', 'fuerza', 'defensa', 'velocidad'];
+        this.puntajePorAtributo = 1000;
+        this.tiposDeDatoPorAtributo = {
+            entrenador: 'string',
+            nombre: 'string',
+            evolucion: 'number',
+            vida: 'number',
+            energia: 'number',
+            energiaLimite: 'number',
+            fuerza: 'number',
+            defensa: 'number',
+            velocidad: 'number',
+            dañoRecibido: 'number'
+        }
+        this.listaAtributosEstado = [
+            'entrenador',
+            'nombre',
+            'evolucion',
+            'vida',
+            'energia',
+            'energiaLimite',
+            'fuerza',
+            'defensa',
+            'velocidad',
+            'dañoRecibido'
+        ];
+        this.tiposDePokemonAceptados = ['agua','fuego','electricidad','planta'];
+    }
+    puntajeMaximoPermitido() {
+        return this.atributosDePokemon.length * this.puntajePorAtributo
+    }
+    multiplicadorDeAtaque(tipoDeAtaque) {
+        return _.get(this.multiplicadoresDeAtaque, tipoDeAtaque)
+    }
 }
 
 module.exports = new config()
