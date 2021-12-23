2745d7dd43f1ba6fecc0fc810489e0ce37ebcefc
Gaston Schvarchman <janeleker@gmail.com>
2021-12-22 23:36:17 -0300

    Modifico Pokemon

--- battle elements/Pokemons/Pokemon.js
+++ battle elements/Pokemons/Pokemon.js
@@ -3,79 +3,70 @@ const config = require('../config')
 const relator = require('../relator')
 
 class Pokemon {
-	constructor({entrenador,nombre,tipoDePokemon,evolucion,vida,energia,fuerza,defensa,velocidad}){
-		this.entrenador = entrenador;
-		this.nombre = nombre;
-		this.tipoDePokemon = tipoDePokemon;
-		this.evolucion = evolucion;
-		this.vida = vida * tipoDePokemon.multiplicadorDeAtributo('vida');
-		this.energia = energia * tipoDePokemon.multiplicadorDeAtributo('energia');
-		this.energiaLimite = energia * tipoDePokemon.multiplicadorDeAtributo('energia');
-		this.fuerza = fuerza * tipoDePokemon.multiplicadorDeAtributo('fuerza');
-		this.defensa = defensa * tipoDePokemon.multiplicadorDeAtributo('defensa');
-		this.velocidad = velocidad * tipoDePokemon.multiplicadorDeAtributo('velocidad');
-		this.dañoRecibido = 0;
-	}
-	miTipo(){
-		return this.tipoDePokemon.nombre
-	}
-	factorDeEvolución(){
-		return Math.sqrt(this.evolucion)
-	}
-	vitalidad(){
-		return _.max([this.vida - this.dañoRecibido,0])
-	}
-	dañoDeAtaque(tipoDeAtaque){
-		return this.fuerza*config.multiplicadorDeAtaque(tipoDeAtaque)* this.factorDeEvolución()
-	}
-	energiaSuficiente(tipoDeAtaque){
-		return this.energia > this.energiaParaAtaque(tipoDeAtaque)
-	}
-	energiaParaAtaque(tipoDeAtaque){
-		return this.tipoDePokemon.energiaParaAtaque(tipoDeAtaque)
-	}
-	atacar(unPokemon,tipoDeAtaque){
-		if(this.energiaSuficiente(tipoDeAtaque)) {
-			this.disminuirEnergia(this.energiaParaAtaque(tipoDeAtaque))
-			unPokemon.recibirDaño(this.dañoDeAtaque(tipoDeAtaque))
-		}else{	
-			this.desmayarse();
-		}
-		
-	}
-	entrenarAtaqueFuerte(){
-		return 'Entrenamiento ataque fuerte completado'
-	}
-	entrenarAtaqueMaximo(){
-		return 'Entrenamiento ataque maximo completado'
-	}
-	deTipo(propertyPath){
-		return _.get(this.tipoDePokemon,propertyPath)
-	}
-	dañoARecibir(unDaño){
-      return unDaño - (this.defensa * this.factorDeEvolución())
-  	}
-	recibirDaño(unDaño){
-   		const __dañoDespuesDeDefensa = unDaño => {
-     		return _.max([0,this.dañoARecibir(unDaño)])
-    	}
-		this.dañoRecibido += __dañoDespuesDeDefensa(unDaño) ;
-	}
-	disminuirEnergia(unaEnergia){
-		this.energia -= unaEnergia
-	}
-	velocidadDeAtaque(){
-		return _.random(0.7,1,0) * this.velocidad
-	}
-	recuperarEnergia(){
-		this.energia = _.min([this.energia+2000,this.energiaLimite]);
-	}
-	desmayarse(){
-		this.energia = _.min([this.energia+750,this.energiaLimite]);
-	}
-	esAtaqueMortal(pokemonOponente,tipoDeAtaque){
-		return pokemonOponente.vitalidad() <= pokemonOponente.dañoARecibir(this.dañoDeAtaque(tipoDeAtaque))
-	}
+    constructor({ entrenador, nombre, tipoDePokemon, evolucion, vida, energia, fuerza, defensa, velocidad }) {
+        this.entrenador = entrenador;
+        this.nombre = nombre;
+        this.tipoDePokemon = require(`../TiposDePokemon/Tipos/${tipoDePokemon}`);
+        this.evolucion = evolucion;
+        this.vida = vida * this.tipoDePokemon.multiplicadorDeAtributo('vida');
+        this.energia = energia * this.tipoDePokemon.multiplicadorDeAtributo('energia');
+        this.fuerza = fuerza * this.tipoDePokemon.multiplicadorDeAtributo('fuerza');
+        this.defensa = defensa * this.tipoDePokemon.multiplicadorDeAtributo('defensa');
+        this.velocidad = velocidad * this.tipoDePokemon.multiplicadorDeAtributo('velocidad');
+        this.dañoRecibido = 0;
+    }
+    miTipo() {
+        return this.tipoDePokemon.nombre
+    }
+    factorDeEvolución() {
+        return Math.sqrt(this.evolucion)
+    }
+    vitalidad() {
+        return _.max([this.vida - this.dañoRecibido, 0])
+    }
+    dañoDeAtaque(tipoDeAtaque) {
+        return this.fuerza * config.multiplicadorDeAtaque(tipoDeAtaque) * this.factorDeEvolución()
+    }
+    energiaParaAtaque(tipoDeAtaque) {
+        return this.tipoDePokemon.energiaParaAtaque(tipoDeAtaque)
+    }
+    atacar(unPokemon, tipoDeAtaque) {
+        this.disminuirEnergia(this.energiaParaAtaque(tipoDeAtaque))
+        unPokemon.recibirDaño(this.dañoDeAtaque(tipoDeAtaque))
+    }
+    entrenarAtaqueFuerte() {
+        return 'Entrenamiento ataque fuerte completado'
+    }
+    entrenarAtaqueMaximo() {
+        return 'Entrenamiento ataque maximo completado'
+    }
+    deTipo(propertyPath) {
+        return _.get(this.tipoDePokemon, propertyPath)
+    }
+    dañoARecibir(unDaño) {
+        return unDaño - (this.defensa * this.factorDeEvolución())
+    }
+    recibirDaño(unDaño) {
+        const __dañoDespuesDeDefensa = unDaño => {
+            return _.max([0, this.dañoARecibir(unDaño)])
+        }
+        this.dañoRecibido += __dañoDespuesDeDefensa(unDaño);
+    }
+    disminuirEnergia(unaEnergia) {
+        this.energia -= unaEnergia
+    }
+    velocidadDeAtaque() {
+        return _.random(0.7, 1, 0) * this.velocidad
+    }
+    recuperarEnergia(energiaLimite) {   
+        this.energia = _.min([this.energia + this.tipoDePokemon.energiaDeRecuperacion, energiaLimite]);
+    }
+    desmayarse(energiaLimite) {
+        this.energia = _.min([this.energia + config.energiaDeDesmayo, energiaLimite]);
+    }
+    esAtaqueMortal(pokemonOponente, tipoDeAtaque) {
+        return pokemonOponente.vitalidad() <= pokemonOponente.dañoARecibir(this.dañoDeAtaque(tipoDeAtaque))
+    }
 }
 
 module.exports = Pokemon
