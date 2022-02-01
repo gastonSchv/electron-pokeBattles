f3871c8a6b30fd0f15d678d56467fbfaa29033ef
Gaston Schvarchman <janeleker@gmail.com>
2022-02-01 00:38:07 -0300

    Homogeinizo consumo energetico para tipo normal

--- battle elements/TiposDePokemon/Tipos/normal.js
+++ battle elements/TiposDePokemon/Tipos/normal.js
@@ -3,6 +3,6 @@ const TipoDePokemon = require('../TipoDePokemon')
 module.exports = new TipoDePokemon({
   nombre:'normal',
   multiplicadoresDeAtributo: {vida:50,energia:3,fuerza:2,defensa:3,velocidad:2},
-  energiaParaAtaques: {basico:150,medio:300,fuerte:450,maximo:600},
+  energiaParaAtaques: {basico:250,medio:500,fuerte:750,maximo:1000},
   energiaDeRecuperacion: 1500
 })
