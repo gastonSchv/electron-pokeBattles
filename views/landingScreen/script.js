8a20a0d8a2793d33edeea336f6b87facedd7c0cb
Gaston Schvarchman <janeleker@gmail.com>
2022-02-10 00:39:19 -0300

    Verifico Pokemon antes de comenzar y agrego detalle error si es necesario

--- views/landingScreen/script.js
+++ views/landingScreen/script.js
@@ -1,77 +1,110 @@
 const { ipcRenderer } = require('electron')
 const _ = require('lodash')
 const util = require('../utils/util')
+const SystemError = require('../../error management/SystemError')
+
 
 let musicaDeBatallaPrendida = true
 let botonesDeJuego = []
 
 function funcionesDeInicio() {
-    botonesDeJuego = [botonBatalla,botonJuezDeBatalla,botonMiPokemon,botonCentroDeEntrenamiento]
+    botonesDeJuego = [botonBatalla, botonJuezDeBatalla, botonMiPokemon, botonCentroDeEntrenamiento]
     util.deshabilitarBotones(botonesDeJuego)
-    ipcRenderer.send('altaDeScreen:landingScreen',{})
+    ipcRenderer.send('altaDeScreen:landingScreen', {})
     musicaDeBatalla.volume = 1
     musicaDeBatalla.loop = true
     util.crearBotonCerradoConEstilo(contenedor)
     prenderMusica()
 }
+
 function apagarMusica() {
-    util.apagarMusica(musicaDeBatalla,musicaDeBatallaImg)   
+    util.apagarMusica(musicaDeBatalla, musicaDeBatallaImg)
 }
 
 function prenderMusica() {
-    util.prenderMusica(musicaDeBatalla,musicaDeBatallaImg)
+    util.prenderMusica(musicaDeBatalla, musicaDeBatallaImg)
 }
 
 function cambiarEstadoMusicaDeBatalla() {
-    util.cambiarEstadoMusicaDeBatalla(musicaDeBatalla,musicaDeBatallaPrendida,musicaDeBatallaImg)
-    musicaDeBatallaPrendida? musicaDeBatallaPrendida=false:musicaDeBatallaPrendida=true
+    util.cambiarEstadoMusicaDeBatalla(musicaDeBatalla, musicaDeBatallaPrendida, musicaDeBatallaImg)
+    musicaDeBatallaPrendida ? musicaDeBatallaPrendida = false : musicaDeBatallaPrendida = true
 }
+
 function abrirPantallaDeBatalla() {
     apagarMusica()
     ipcRenderer.send('screens:battleScreen', {})
 }
-function abrirModalSelectorDeEnemigo(){
+
+function abrirModalSelectorDeEnemigo() {
     apagarMusica()
-    ipcRenderer.send('screens:selectorDeEnemigoScreen',{})
+    ipcRenderer.send('screens:selectorDeEnemigoScreen', {})
 }
+
 function abrirModalConfiguracion() {
     ipcRenderer.send('screens:configurationScreen', {})
 }
-function abrirModalMiPokemon(){
-    ipcRenderer.send('screens:miPokemonScreen',{})
+
+function abrirModalMiPokemon() {
+    ipcRenderer.send('screens:miPokemonScreen', {})
 }
+
 function abrirModalDeJuezDeBatalla() {
     apagarMusica()
     ipcRenderer.send('screens:juezDeBatallaScreen', {})
 }
-function cerrarPantalla(){
-	window.close()
+
+function cerrarPantalla() {
+    window.close()
 }
-function consultarRutaValida(){
-    ipcRenderer.send('rutaValida',{})
+
+function consultarRutaValida() {
+    ipcRenderer.send('rutaValida', {})
 }
-function abrirModalCentroDeEntrenamiento(){
-    ipcRenderer.send('screens:centroDeEntrenamientoScreen', {})    
+
+function abrirModalCentroDeEntrenamiento() {
+    ipcRenderer.send('screens:centroDeEntrenamientoScreen', {})
 }
-ipcRenderer.on('altaDeScreen:landingScreen',(event,data) => {
-    if(data.ruta){
-      const pokemon = require(data.ruta)
-      util.habilitarBotones(botonesDeJuego)
-      util.colocarFotoMiniaturaMiPokemon(iconoPokemon,pokemon)  
+function borrarBotonesDeError(){
+    const botonesDeError = document.getElementsByClassName("botonDetalleError")
+    _.forEach(botonesDeError, botonDeError => {
+        botonDeError.style.opacity = 0
+        botonDeError.disabled = true
+    })
+}
+function verificarPokemonAntesDeComenzar(data) {
+    botonesDeJuego = [botonBatalla, botonJuezDeBatalla, botonMiPokemon, botonCentroDeEntrenamiento]
+    if (data.ruta) {
+        try {
+            const pokemon = require(data.ruta)
+            util.habilitarBotones(botonesDeJuego)
+            util.colocarFotoMiniaturaMiPokemon(iconoPokemon, pokemon)
+            borrarBotonesDeError()
+        } catch (err) {
+            const systemError = new SystemError(err)
+            util.deshabilitarBotones(botonesDeJuego)
+            const botonConfiguracionDiv = document.getElementById("botonConfiguracionDiv")
+            util.habilitarDetalleResultadoFallido(botonConfiguracionDiv,"mainError",systemError)
+            util.colocarFotoMiniaturaMiPokemon(iconoPokemon, '')
+        }
     }
+}
+function mostrarDetalleError(errMessage,recommendations,originalErrorMessage){//sumar localizacion
+    ipcRenderer.send('detalleDeError',{errMessage,recommendations,originalErrorMessage})
+}
+ipcRenderer.on('altaDeScreen:landingScreen', (event, data) => {
+    verificarPokemonAntesDeComenzar(data)
 })
-ipcRenderer.on('bloqueoBotonesDeJuego',(event,data) => {
-    botonesDeJuego = [botonBatalla,botonMiPokemon,botonCentroDeEntrenamiento]
+ipcRenderer.on('bloqueoBotonesDeJuego', (event, data) => {
+    botonesDeJuego = [botonBatalla, botonMiPokemon, botonCentroDeEntrenamiento]
     if (!data.deshabilitarBotones) {
-        consultarRutaValida()      
-    }else{
-        util.deshabilitarBotones(botonesDeJuego)      
+        consultarRutaValida()
+    } else {
+        util.deshabilitarBotones(botonesDeJuego)
     }
 })
-ipcRenderer.on('rutaValida',(event,data) => {
+ipcRenderer.on('rutaValida', (event, data) => {
     util.habilitarBotones(botonesDeJuego.concat(botonJuezDeBatalla))
 })
-ipcRenderer.on('altaDeScreen:configuracion',(event,data) => {
-      const pokemon = require(data.ruta)
-      util.colocarFotoMiniaturaMiPokemon(iconoPokemon,pokemon)  
+ipcRenderer.on('altaDeScreen:configuracion', (event, data) => {
+    verificarPokemonAntesDeComenzar(data)
 })
