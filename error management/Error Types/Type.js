7d14a2dc7a2f83fe27d45f1a3fcfee0ccae50736
Gaston Schvarchman <janeleker@gmail.com>
2022-02-13 11:47:10 -0300

    Ahora type es responsable por la localizacion

--- error management/Error Types/Type.js
+++ error management/Error Types/Type.js
@@ -1,30 +1,65 @@
-const SyntaxError = require('./SyntaxError')
-const ReferenceError = require('./ReferenceError')
-const _ = require('lodash')
+const SyntaxError = require('./SyntaxError');
+const ReferenceError = require('./ReferenceError');
+const TypeError = require('./TypeError');
+const MissingModule = require('./MissingModuleError')
+const _ = require('lodash');
+const path = require('path');
 
 class Type {
 	constructor(err){
 		this.err = err;
 		this.referenceError = new ReferenceError(err);
 		this.syntaxError = new SyntaxError(err);
+		this.typeError = new TypeError(err);
+		this.missingModule = new MissingModule(err)
 	}
 	availableTypes(){
 		return {
 			SyntaxError:this.syntaxError,
-			ReferenceError:this.referenceError
+			ReferenceError:this.referenceError,
+			TypeError:this.typeError,
+			MissingModule: this.missingModule
 		}
 	}
 	errorTypeName(){
-		return _.find(_.keys(this.availableTypes()),key => _.includes(this.err.stack,key))
+		return _.find(_.keys(this.availableTypes()),key => {
+			return _.includes(this.err.stack,_.get(this.availableTypes(),key).identifier)
+		})
 	}
 	errorType(){
 		return _.get(this.availableTypes(),this.errorTypeName())
 	}
 	errorPosition(){
-		return this.errorType().errorPosition()
+		try {
+			return this.errorType().errorPosition()
+		}catch(err){
+			return {linea:"",columna:""}
+		}
 	}
 	errorRoute(){
-		return this.errorType().errorRoute()
+		try {
+			return this.errorType().errorRoute()
+		}catch(err){
+			return ""
+		}
+	}
+	isErrorInAppFiles(){
+		return _.some(util.obtenerNombresDeArchivos(path.join(__dirname,'..','..')),fileName => _.includes(this.errorRoute(),fileName))
+	}
+	errorLocalization(){
+		try {
+			const {linea,columna} = this.errorPosition();
+			const columnMessage = columna? ` columna ${columna}`:"";
+			const lineMessage =  linea? ` linea ${linea}`:"";
+			const fileMessage = this.errorRoute()? `en el archivo ${this.errorRoute()}`:"";
+			if(columnMessage||lineMessage||fileMessage){
+				const localizationString = `<>Localizacion del error*Revisa ${lineMessage} ${columnMessage}  ${fileMessage} , al parecer ahí está el problema`
+				return localizationString //this.isErrorInAppFiles()? "":localizationString
+			}
+			return ""
+		}catch(err){
+			return ""
+		}
 	}
 }
 
