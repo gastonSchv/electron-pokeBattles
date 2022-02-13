d55b35cedd38434da6433c390ad5dc0404e7c7fc
Gaston Schvarchman <janeleker@gmail.com>
2022-02-13 11:40:24 -0300

    Delego localizacion en el Tpy

--- error management/BasicError.js
+++ error management/BasicError.js
@@ -4,7 +4,7 @@ const Type = require('./Error Types/Type')
 
 class BasicError {
 	constructor(error){
-		this.originalError = error;
+		this.originalError = error
 		this.prettifier = _.find(prettifiers, prettifier => prettifier.isSuitable(this.message()))
 		this.type = new Type(error)
 	}
@@ -14,25 +14,18 @@ class BasicError {
 	prettyMessage(){
 		return this.prettifier?this.prettifier.prettify(this.message()):this.message()
 	}
+	cleanedErrorStack(){
+		return this.originalError.stack.split('\\').join('/').replace(/-|\"|\'|\`|\r?\n|\r/gi,'')
+	}
 	generalRecommendations(){
-		return `Error Original*${this.originalError}<>Localizacion del error*${this.errorLocalization()}`
+		const originalError = this.isCreatedError ? "":`<>Error Original*${this.cleanedErrorStack()}`//ver para ocultar rutas propios en mensaje de error original
+		return `${originalError}${this.type.errorLocalization()}`
 	}
 	recommendations(){
 		if(this.prettifier){
-			return `${this.prettifier.recommendationsString(this.message())}<>${this.generalRecommendations()}`		
+			return `${this.prettifier.recommendationsString(this.message())}${this.generalRecommendations()}`		
 		}
 		return this.generalRecommendations()
-	}
-	errorPosition(){
-		return this.type.errorPosition()
-	}
-	errorRoute(){
-		return this.type.errorRoute()
-	}
-	errorLocalization(){
-		const err = this.originalError
-		const {linea,columna} = this.errorPosition();
-		return `Revisa la linea ${linea} y columna ${columna} del archivo ${this.errorRoute()} , al parecer ahí está el problema`
 	}
 }
 
