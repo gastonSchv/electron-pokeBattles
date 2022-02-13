0b4739997634057cb2e099f85c38fbd05e701459
Gaston Schvarchman <janeleker@gmail.com>
2022-02-13 11:47:45 -0300

    Agrego mensajes de resultados incorrectos

--- evaluation management/informacionEvaluaciones.js
+++ evaluation management/informacionEvaluaciones.js
@@ -1,40 +1,50 @@
 module.exports = [{
         id: 'atributosNecesarios',
-        mensajeResultadoCorrecto: 'Tu pokemon cuenta con los atributos requeridos'
+        mensajeResultadoCorrecto: 'Tu pokemon cuenta con los atributos requeridos',
+        mensajeResultadoIncorrecto: 'Tu pokemon No cuenta con los atributos requeridos'        
     },
     {
         id: 'atributosConValoresPermitidos',
-        mensajeResultadoCorrecto: 'Tu pokemon cuenta con valores permitidos en todos sus atributos'
+        mensajeResultadoCorrecto: 'Tu pokemon cuenta con valores permitidos en todos sus atributos',
+        mensajeResultadoIncorrecto: 'Tu pokemon No cuenta con valores permitidos en todos sus atributos'        
     }, {
         id: 'puntoDeAtributosMaximoPermitido',
-        mensajeResultadoCorrecto: 'Tu pokemon no excede el límite de puntos de atributos permitidos'
+        mensajeResultadoCorrecto: 'Tu pokemon no excede el límite de puntos de atributos permitidos',
+        mensajeResultadoIncorrecto: 'Tu pokemon excede el límite de puntos de atributos permitidos'        
     },
     {
         id: 'metodosNecesarios',
-        mensajeResultadoCorrecto: 'Tu pokemon cuenta con todos los metodos requeridos'
+        mensajeResultadoCorrecto: 'Tu pokemon cuenta con todos los metodos requeridos',
+        mensajeResultadoIncorrecto: 'Tu pokemon No cuenta con todos los metodos requeridos'        
     },
     {
         id: 'retornoVitalidadCorrecto',
-        mensajeResultadoCorrecto: 'Tu pokemon retorna el valor correcto al invocar su metodo vitalidad()'
+        mensajeResultadoCorrecto: 'Tu pokemon retorna el valor correcto al invocar su metodo vitalidad()',
+        mensajeResultadoIncorrecto: 'Tu pokemon No retorna el valor correcto al invocar su metodo vitalidad()'        
     },
     {
         id: 'retornoMiTipoValido',
-        mensajeResultadoCorrecto: 'Tu pokemon retorna el valor correcto al invocar su metodo miTipo()'
+        mensajeResultadoCorrecto: 'Tu pokemon retorna el valor correcto al invocar su metodo miTipo()',
+        mensajeResultadoIncorrecto: 'Tu pokemon No retorna el valor correcto al invocar su metodo miTipo()'        
     }, 
     {
         id: 'dañoDeAtaquesDisponiblesCorrecto',
-        mensajeResultadoCorrecto: 'Tu pokemon genera el daño correcto en todos sus ataques disponibles'
+        mensajeResultadoCorrecto: 'Tu pokemon genera el daño correcto en todos sus ataques disponibles',
+        mensajeResultadoIncorrecto: 'Tu pokemon No genera el daño correcto en todos sus ataques disponibles'        
     },
     {
         id: 'energiaConsumidaPorAtaqueCorrecto',
-        mensajeResultadoCorrecto: 'Tu pokemon consume la energia correcta en todos sus ataques disponibles'
+        mensajeResultadoCorrecto: 'Tu pokemon consume la energia correcta en todos sus ataques disponibles',
+        mensajeResultadoIncorrecto: 'Tu pokemon No consume la energia correcta en todos sus ataques disponibles'        
     },
     {
         id: 'desmayarseCorrectamente',
-        mensajeResultadoCorrecto: 'Tu pokemon se demaya correctamente'
+        mensajeResultadoCorrecto: 'Tu pokemon se demaya correctamente',
+        mensajeResultadoIncorrecto: 'Tu pokemon No se demaya correctamente'        
     },
     {
         id: 'recuperarEnergiaCorrectamente',
-        mensajeResultadoCorrecto: 'Tu pokemon recupera energia correctamente'
+        mensajeResultadoCorrecto: 'Tu pokemon recupera energia correctamente',
+        mensajeResultadoIncorrecto: 'Tu pokemon No recupera energia correctamente'        
     }
 ]
