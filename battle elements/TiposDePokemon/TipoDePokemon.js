b866a2542c5da0fbc7c9d15e8d6386d685cfafe4
Gaston Schvarchman <janeleker@gmail.com>
2022-03-28 00:29:23 -0300

    Agrego ataque especial generico a TipoDePokemon

--- battle elements/TiposDePokemon/TipoDePokemon.js
+++ battle elements/TiposDePokemon/TipoDePokemon.js
@@ -13,6 +13,17 @@ class TipoDePokemon {
 	energiaParaAtaqueEspecial(pokemon){
 		return pokemon.energia	
 	}
+	atacarEspecial(pokemonEnemigo, miPokemon) {
+        const correctorPorEnergiaBaja = miPokemon.energia / this.energiaParaAtaques.maximo <= 1 ? 0.7 : 1
+        const _multiplicadorPorEnergia = () => {
+            return (1.15 + 1.03 * Math.log(miPokemon.energia / this.energiaParaAtaques.maximo)) * correctorPorEnergiaBaja
+        }
+        const _deterioroDeAtaqueEspecial = () => {
+            return _.round(_.max([0, miPokemon.danoDeAtaque('maximo') * _multiplicadorPorEnergia()]))
+        }
+        pokemonEnemigo.recibirDeterioro(_deterioroDeAtaqueEspecial())
+        miPokemon.disminuirEnergia('especial')
+    }
 	energiaParaAtaque(tipoDeAtaque,pokemon){
 		if(tipoDeAtaque == 'especial'){
 			return this.energiaParaAtaqueEspecial(pokemon)
